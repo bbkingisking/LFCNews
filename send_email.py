b2f712a07500328ae11e02b97448660ab5df9cef
whitman <whitman@fake.com>
2025-04-08 09:51:20 +0100

    Modified send_email to work with multiple email addresses in .env

--- send_email.py
+++ send_email.py
@@ -10,16 +10,19 @@ load_dotenv()
 
 def send_email(message):
     try:
+        recipients = [r.strip() for r in os.getenv("RECIPIENT_EMAIL_ADDRESS").split(",")]
+
         msg = MIMEMultipart()
         msg['From'] = os.getenv("EMAIL_ADDRESS")
-        msg['To'] = os.getenv("RECIPIENT_EMAIL_ADDRESS")
+        msg['To'] = ", ".join(recipients)
         msg['Subject'] = "LFC News summary"
         msg.attach(MIMEText(message, 'plain'))
 
         with smtplib.SMTP("smtp.mail.me.com", 587) as server:
             server.starttls()
             server.login(os.getenv("EMAIL_ADDRESS"), os.getenv("EMAIL_PASSWORD"))
-            server.send_message(msg)
+            server.sendmail(os.getenv("EMAIL_ADDRESS"), recipients, msg.as_string())
 
     except Exception as e:
         raise
+
